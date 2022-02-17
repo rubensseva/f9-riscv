eba67a30622d6ed09171f99d2e2cf7e1d1913afd
rubensseva <rubenss@stud.ntnu.no>
2022-02-17 13:23:34 +0100

    Fix bss issue and allow for linker relaxation

--- kernel/entry.s
+++ kernel/entry.s
@@ -1,18 +1,18 @@
-	# qemu -kernel loads the kernel at 0x80000000
-    # and causes each CPU to jump there.
-    # kernel.ld causes the following code to
-    # be placed at 0x80000000.
 .section __l4_start_section
 .global idle_stack_end
 .global _entry
 _entry:
-	# set up a stack for C.
-    # stack0 is declared in start.c,
-    # with a 4096-byte stack per CPU.
-    # sp = stack0 + (hartid * 4096)
+    # Load __global_pointer$ (from linker script) into gp.
+    # Should allow for linker relaxation
+    .option push
+    .option norelax
+    1:auipc gp, %pcrel_hi(__global_pointer$)
+    addi  gp, gp, %pcrel_lo(1b)
+    .option pop
+
+    # Set up a stack for C.
     la sp, kernel_stack_end
     li a0, 1024*4
-	# jump to start() in start.c
     call __l4_start
 spin:
     j spin
