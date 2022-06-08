0513195d988e0817372f3f0de148035c6004d2fd
rubensseva <rubenss@stud.ntnu.no>
2022-06-08 20:46:15 +0200

    Create functions for hoppus and pingpong

--- user/root_thread.c
+++ user/root_thread.c
@@ -10,7 +10,7 @@
 #include <F9.h>
 #include <user_thread.h>
 #include <message.h>
-#include <user_stdio.h>
+#include <user_thread_log.h>
 #include <ping_pong.h>
 
 
@@ -21,20 +21,44 @@ __USER_DATA uint32_t timer_mem_size = 0xFFF;
 
 extern void* current_utcb;
 
-/* Kip_ptr and utcb_ptr will be passed through a0 and a1 by create_root_thread() */
-void __USER_TEXT root_thread(kip_t *kip_ptr, utcb_t *utcb_ptr)
-{
+void timed_sleep() {
+    ipc_time_t sleep = {.raw = 0};
+    sleep.period.m = 500;
 
+    sleep.period.e = 5;
+    user_log_printf("root sleeping for %d\n", (sleep.period.m << sleep.period.e));
+    timer_init();
+    TIMER_START();
+    L4_Sleep(sleep);
+    TIMER_LATCH();
+    int res = timer_get();
+    user_log_puts("done sleeping\n");
+    user_log_printf("timed sleep: %d\n", res);
+    int useconds = timer_counter_to_microseconds(res);
+    user_log_printf("timed sleep us: %dus\n", useconds);
+
+    sleep.period.e = 4;
+    user_log_printf("root sleeping for %d\n", (sleep.period.m << sleep.period.e));
+    timer_reset();
+    TIMER_START();
+    L4_Sleep(sleep);
+    TIMER_LATCH();
+    res = timer_get();
+    user_log_puts("done sleeping\n");
+    user_log_printf("timed sleep: %d\n", res);
+    useconds = timer_counter_to_microseconds(res);
+    user_log_printf("timed sleep us: %dus\n", useconds);
+}
+
+
+void run_hoppus(kip_t *kip_ptr, utcb_t *utcb_ptr) {
     L4_ThreadId_t myself = {.raw = utcb_ptr->t_globalid};
-    L4_ThreadId_t user_thread_id = {.raw = TID_TO_GLOBALID(24)};
-
-
-
     char *free_mem = (char *) get_free_base(kip_ptr);
 
+    L4_ThreadId_t user_thread_id = {.raw = TID_TO_GLOBALID(24)};
 
+    user_log_printf("Starting hoppus thread with id %d\n", user_thread_id);
 
-    /***** Hoppus thread *****/
     /* Create user thread */
     /* Let threadid = spaceid, to tell the kernel to create a new address space, instead of sharing an existing one. */
     L4_ThreadControl(user_thread_id, user_thread_id, L4_nilthread, myself, free_mem);
@@ -70,104 +94,91 @@ void __USER_TEXT root_thread(kip_t *kip_ptr, utcb_t *utcb_ptr)
     L4_MsgPut(&msg, 0, 5, msgs, 0, NULL);
     L4_MsgLoad(&msg);
     L4_Ipc(user_thread_id, myself, 0, (L4_ThreadId_t *)0);
+}
 
 
-
+void run_pingpong(kip_t *kip_ptr, utcb_t *utcb_ptr) {
+    L4_ThreadId_t myself = {.raw = utcb_ptr->t_globalid};
+    char *free_mem = (char *) get_free_base(kip_ptr);
 
     /***** Ping thread *****/
-    /* { */
-    /*     ping_id = (L4_ThreadId_t){.raw = TID_TO_GLOBALID(50)}; */
+    {
+        ping_id = (L4_ThreadId_t){.raw = TID_TO_GLOBALID(50)};
+        user_log_printf("Starting ping thread with id %d\n", ping_id);
 
 
-    /*     L4_ThreadControl(ping_id, ping_id, L4_nilthread, myself, free_mem); */
+        L4_ThreadControl(ping_id, ping_id, L4_nilthread, myself, free_mem);
 
-    /*     map_user_text(kip_ptr, ping_id); */
+        map_user_text(kip_ptr, ping_id);
 
-    /*     L4_map((uint32_t)&ping_thread_stack_start, */
-    /*         (char *)&ping_thread_stack_end - (char *)&ping_thread_stack_start, */
-    /*         ping_id); */
-    /*     L4_map((uint32_t)&user_threads_data_start, */
-    /*         (char *)&user_threads_data_end - (char *)&user_threads_data_start, */
-    /*         ping_id); */
-    /*     L4_map(timer_mem_base, timer_mem_size, ping_id); */
+        L4_map((uint32_t)&ping_thread_stack_start,
+            (char *)&ping_thread_stack_end - (char *)&ping_thread_stack_start,
+            ping_id);
+        L4_map((uint32_t)&user_threads_data_start,
+            (char *)&user_threads_data_end - (char *)&user_threads_data_start,
+            ping_id);
+        L4_map(timer_mem_base, timer_mem_size, ping_id);
 
-    /*     L4_Msg_t msg; */
-    /*     L4_MsgClear(&msg); */
-    /*     L4_Word_t msgs[5] = { */
-    /*         (L4_Word_t) ping, */
-    /*         (L4_Word_t) &ping_thread_stack_end, */
-    /*         (L4_Word_t)(((uint32_t) &ping_thread_stack_end) - ((uint32_t) &ping_thread_stack_start)), // stack size */
-    /*         0, */
-    /*         0 */
-    /*     }; */
+        L4_Msg_t msg;
+        L4_MsgClear(&msg);
+        L4_Word_t msgs[5] = {
+            (L4_Word_t) ping,
+            (L4_Word_t) &ping_thread_stack_end,
+            (L4_Word_t)(((uint32_t) &ping_thread_stack_end) - ((uint32_t) &ping_thread_stack_start)), // stack size
+            0,
+            0
+        };
 
-    /*     L4_MsgPut(&msg, 0, 5, msgs, 0, NULL); */
-    /*     L4_MsgLoad(&msg); */
-    /*     L4_Ipc(ping_id, myself, 0, (L4_ThreadId_t *)0); */
-    /* } */
+        L4_MsgPut(&msg, 0, 5, msgs, 0, NULL);
+        L4_MsgLoad(&msg);
+        L4_Ipc(ping_id, myself, 0, (L4_ThreadId_t *)0);
 
+    }
 
 
     /***** Pong thread *****/
-    /* { */
-    /*     pong_id = (L4_ThreadId_t){.raw = TID_TO_GLOBALID(51)}; */
+    {
+        pong_id = (L4_ThreadId_t){.raw = TID_TO_GLOBALID(51)};
+        user_log_printf("Starting pong thread with id %d\n", pong_id);
 
-    /*     L4_ThreadControl(pong_id, pong_id, L4_nilthread, myself, free_mem); */
+        L4_ThreadControl(pong_id, pong_id, L4_nilthread, myself, free_mem);
 
-    /*     map_user_text(kip_ptr, pong_id); */
+        map_user_text(kip_ptr, pong_id);
 
-    /*     L4_map((uint32_t)&pong_thread_stack_start, */
-    /*         (char *)&pong_thread_stack_end - (char *)&pong_thread_stack_start, */
-    /*         pong_id); */
-    /*     L4_map((uint32_t)&user_threads_data_start, */
-    /*            (char *)&user_threads_data_end - (char *)&user_threads_data_start, */
-    /*         pong_id); */
+        L4_map((uint32_t)&pong_thread_stack_start,
+            (char *)&pong_thread_stack_end - (char *)&pong_thread_stack_start,
+            pong_id);
+        L4_map((uint32_t)&user_threads_data_start,
+               (char *)&user_threads_data_end - (char *)&user_threads_data_start,
+            pong_id);
 
-    /*     L4_Msg_t msg; */
-    /*     L4_MsgClear(&msg); */
+        L4_Msg_t msg;
+        L4_MsgClear(&msg);
 
-    /*     L4_Word_t msgs[5] = { */
-    /*         (L4_Word_t) pong, */
-    /*         (L4_Word_t) &pong_thread_stack_end, */
-    /*         (L4_Word_t)(((uint32_t) &pong_thread_stack_end) - ((uint32_t) &pong_thread_stack_start)), // stack size */
-    /*         0, */
-    /*         0 */
-    /*     }; */
+        L4_Word_t msgs[5] = {
+            (L4_Word_t) pong,
+            (L4_Word_t) &pong_thread_stack_end,
+            (L4_Word_t)(((uint32_t) &pong_thread_stack_end) - ((uint32_t) &pong_thread_stack_start)), // stack size
+            0,
+            0
+        };
 
-    /*     L4_MsgPut(&msg, 0, 5, msgs, 0, NULL); */
-    /*     L4_MsgLoad(&msg); */
-    /*     L4_Ipc(pong_id, myself, 0, (L4_ThreadId_t *)0); */
-    /* } */
+        L4_MsgPut(&msg, 0, 5, msgs, 0, NULL);
+        L4_MsgLoad(&msg);
+        L4_Ipc(pong_id, myself, 0, (L4_ThreadId_t *)0);
+    }
+}
 
 
 
 
-    /* ipc_time_t sleep = {.raw = 0}; */
-    /* sleep.period.m = 500; */
+/* Kip_ptr and utcb_ptr will be passed through a0 and a1 by create_root_thread() */
+void __USER_TEXT root_thread(kip_t *kip_ptr, utcb_t *utcb_ptr)
+{
+    /* run_hoppus(kip_ptr, utcb_ptr); */
+    run_pingpong(kip_ptr, utcb_ptr);
+    /* timed_sleep() */
 
-    /* sleep.period.e = 5; */
-    /* user_printf("root sleeping for %d\n", (sleep.period.m << sleep.period.e)); */
-    /* timer_init(); */
-    /* TIMER_START(); */
-    /* L4_Sleep(sleep); */
-    /* TIMER_LATCH(); */
-    /* int res = timer_get(); */
-    /* user_puts("done sleeping\n"); */
-    /* user_printf("timed sleep: %d\n", res); */
-    /* int useconds = timer_counter_to_microseconds(res); */
-    /* user_printf("timed sleep us: %dus\n", useconds); */
-
-    /* sleep.period.e = 4; */
-    /* user_printf("root sleeping for %d\n", (sleep.period.m << sleep.period.e)); */
-    /* timer_reset(); */
-    /* TIMER_START(); */
-    /* L4_Sleep(sleep); */
-    /* TIMER_LATCH(); */
-    /* res = timer_get(); */
-    /* user_puts("done sleeping\n"); */
-    /* user_printf("timed sleep: %d\n", res); */
-    /* useconds = timer_counter_to_microseconds(res); */
-    /* user_printf("timed sleep us: %dus\n", useconds); */
 
     while (1) {
         /* TODO: Need to use proper timeout object here */
